c4c917e1db6d2c140e93a242af4a283f71dcb5be
Mike Fikes <mike@fikesfarm.com>
2018-07-19 11:25:28 -0400

    Latest patches

--- README.md
+++ README.md
@@ -1,19 +1,24 @@
 # patch-tender
+ClojureScript [JIRA](https://dev.clojure.org/jira/browse/CLJS) contains many candidate patches that have not yet been applied to master.
 
-Check that each ClojureScript patch in `resources/patches.txt` applies:
+`patch-tender` is used to maintain a curated list of patches, applying them in a branch so they can be soak-tested.
+
+The latest set of applied patches are in this branch 
+
+   https://github.com/mfikes/clojurescript/commits/patch-tender-2018-07-19T15 
+
+Branch build status: [![Build Status](https://travis-ci.org/mfikes/clojurescript.svg?branch=patch-tender-2018-07-19T15)](https://travis-ci.org/mfikes/clojurescript) [![Build status](https://ci.appveyor.com/api/projects/status/oggs1yydb8c2t6pa/branch/patch-tender-2018-07-19T15?svg=true)](https://ci.appveyor.com/project/mfikes/clojurescript/branch/patch-tender-2018-07-19T15)
+
+If using `deps.edn` you can depend on this set of patches via
+```clojure
+org.clojure/clojurescript {:git/url "https://github.com/mfikes/clojurescript", :sha "3cf246ac1716a10b2dc4b145297d53c66100de31"}
+```
+
+Or you can clone and build this branch for use in a `lein`- or `boot`-based project:
 
 ```
-plk -Acheck
+$ git clone https://github.com/mfikes/clojurescript -b patch-tender-2018-07-19T15
+$ cd clojurescript
+$ script/build
 ```
-
-Build ClojureScript compiler with patches for select tickets in `resources/tickets.txt`:
-
-```
-plk -Abuild
-```
-
-Build (as above) and then run ClojureScript compiler tests:
-
-```
-plk -Atest
-```
+For more info see https://clojurescript.org/community/building
--- src/patch/tender.cljs
+++ src/patch/tender.cljs
@@ -29,8 +29,26 @@
         (spit cache-file (slurp url)))
       (io/copy cache-file (io/file tmpdir "temp.patch")))))
 
+(defn inst->branch-name-suffix
+  [ts]
+  (let [normalize (fn [n len]
+                    (loop [ns (str n)]
+                      (if (< (count ns) len)
+                        (recur (str "0" ns))
+                        ns)))]
+    (str
+      (.getUTCFullYear ts) "-"
+      (normalize (inc (.getUTCMonth ts)) 2) "-"
+      (normalize (.getUTCDate ts) 2) "T"
+      (normalize (.getUTCHours ts) 2))))
+
+(defn inst->branch-name
+  [ts]
+  (str "patch-tender-" (inst->branch-name-suffix ts)))
+
 (defn -main [& args]
-  (let [test? (= "test" (first args))
+  (let [push? (= "push" (first args))
+        test? (= "test" (first args))
         test-ndx (and test? (when-some [ndx (second args)] (js/parseInt ndx)))
         build? (or test? (= "build" (first args)))
         patch-filter (if build?
@@ -40,33 +58,71 @@
                                    (string/includes? url ticket))
                              tickets)))
                        (constantly true))
-        tmpdir (io/file (string/trim (:out (shell/sh "mktemp" "-d"))))]
+        tmpdir (io/file (string/trim (:out (shell/sh "mktemp" "-d"))))
+        clojurescript-dir (io/file tmpdir "clojurescript")
+        branch-name (inst->branch-name (js/Date.))]
     (with-sh-dir tmpdir
       (shell/sh "git" "clone" "https://github.com/clojure/clojurescript")
+      (with-sh-dir clojurescript-dir
+        (shell/sh "git" "checkout" "-b" branch-name))
       (doseq [url (if test-ndx
                     [(nth (filter patch-filter patches) test-ndx)]
                     (filter patch-filter patches))]
         (when test-ndx (println "Testing" url))
         (fetch-patch tmpdir url)
-        (with-sh-dir (io/file tmpdir "clojurescript")
-          (let [res (if build?
+        (with-sh-dir clojurescript-dir
+          (let [res (if push?
                       (let [res2 (shell/sh "git" "apply" "--check" "../temp.patch")]
                         (if (zero? (:exit res2))
                           (shell/sh "git" "am" "../temp.patch")
                           res2))
                       (shell/sh "git" "apply" "--check" "../temp.patch"))]
             (when-not (zero? (:exit res))
-              (println url "does not apply")))))
-      (when build?
-        (println "Building...")
-        (with-sh-dir (io/file tmpdir "clojurescript")
-          (let [res (shell/sh "script/build")]
-            (println (:err res))
-            (println (:out res)))))
-     (when test?
-        (println "Testing...")
-        (with-sh-dir (io/file tmpdir "clojurescript")
-          (shell/sh "script/bootstrap")
-          (let [res (shell/sh "script/test")]
-            (println (:err res))
-            (println (:out res))))))))
+              (println url "does not apply"))))))
+    (when build?
+      (println "Building...")
+      (with-sh-dir (io/file tmpdir "clojurescript")
+        (let [res (shell/sh "script/build")]
+          (println (:err res))
+          (println (:out res)))))
+    (when test?
+      (println "Testing...")
+      (with-sh-dir clojurescript-dir
+        (shell/sh "script/bootstrap")
+        (let [res (shell/sh "script/test")]
+          (println (:err res))
+          (println (:out res)))))
+    (when push?
+      (println "Pushing...")
+      (with-sh-dir clojurescript-dir
+        (shell/sh "git" "remote" "add" "personal" "git@github.com:mfikes/clojurescript")
+        (shell/sh "git" "push" "personal"))
+      (let [sha (string/trim (:out (shell/sh "git" "rev-parse" "HEAD")))]
+        (spit "README.md"
+          (str
+            "# patch-tender" \newline
+            "ClojureScript [JIRA](https://dev.clojure.org/jira/browse/CLJS) contains many candidate patches that have not yet been applied to master." \newline
+            \newline
+            "`patch-tender` is used to maintain a curated list of patches, applying them in a branch so they can be soak-tested." \newline
+            \newline
+            "The latest set of applied patches are in this branch " \newline
+            \newline
+            "   https://github.com/mfikes/clojurescript/commits/" branch-name " " \newline
+            \newline
+            "Branch build status: " "[![Build Status](https://travis-ci.org/mfikes/clojurescript.svg?branch=" branch-name ")](https://travis-ci.org/mfikes/clojurescript)"
+            " " "[![Build status](https://ci.appveyor.com/api/projects/status/oggs1yydb8c2t6pa/branch/" branch-name "?svg=true)](https://ci.appveyor.com/project/mfikes/clojurescript/branch/" branch-name ")"
+            \newline \newline
+            "If using `deps.edn` you can depend on this set of patches via" \newline
+            "```clojure" \newline
+            'org.clojure/clojurescript " " {:git/url "https://github.com/mfikes/clojurescript"
+                                            :sha     sha} \newline
+            "```" \newline
+            \newline
+            "Or you can clone and build this branch for use in a `lein`- or `boot`-based project:" \newline
+            \newline
+            "```" \newline
+            "$ git clone https://github.com/mfikes/clojurescript -b " branch-name \newline
+            "$ cd clojurescript" \newline
+            "$ script/build" \newline
+            "```" \newline
+            "For more info see https://clojurescript.org/community/building"))))))
