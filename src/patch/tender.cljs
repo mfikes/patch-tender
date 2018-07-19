bdb9d41d0360d18185c95a429e941deaed647078
Mike Fikes <mike@fikesfarm.com>
2018-07-19 11:35:15 -0400

    Update copy; include compare link

--- src/patch/tender.cljs
+++ src/patch/tender.cljs
@@ -103,9 +103,9 @@
             "# patch-tender" \newline
             "ClojureScript [JIRA](https://dev.clojure.org/jira/browse/CLJS) contains many candidate patches that have not yet been applied to master." \newline
             \newline
-            "`patch-tender` is used to maintain a curated list of patches, applying them in a branch so they can be soak-tested." \newline
+            "`patch-tender` is used to maintain a curated list of patches, applying them in a branch so they can be easily soak-tested in downstream projects." \newline
             \newline
-            "The latest set of applied patches are in this branch " \newline
+            "The latest set of [applied patches](https://github.com/clojure/clojurescript/compare/master...mfikes:" branch-name ") are in this branch " \newline
             \newline
             "   https://github.com/mfikes/clojurescript/commits/" branch-name " " \newline
             \newline
